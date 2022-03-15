c729d7525041e091307b4e6f80423716ab4e93ae
TarounCoder <94358556+TarounCoder@users.noreply.github.com>
2022-03-15 15:35:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var first_name = "Ranbir";
+console.log(first_name);
+
+var last_name = "Kapoor";
+console.log(last_name);
+
+var new_name  = first_name.concat(last_name);
+console.log(new_name);
+
+var a = 10;
+console.log(a);
+
+var b = 7;
+console.log(b);
+
+var add = a + b ;
+console.log(add);
